af058fcfdb59d03c77da62c70da1403ede74677f
Subhash Vadaparthi <2300030695@kluniversity.in>
2026-06-20 21:36:31 +0530

    Add: pow-x-n - submission-0

---
+++ Data Structures & Algorithms/pow-x-n/submission-0.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def myPow(self, x, n):
+        if n < 0:
+            x = 1 / x
+            n = -n
+        ans = 1
+        current_product = x
+        while n > 0:
+            if n % 2 == 1:
+                ans *= current_product
+            current_product *= current_product
+            n //= 2
+        return ans
